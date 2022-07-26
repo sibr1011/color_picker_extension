dea703b2cc890ef502176e7fcd8d002fdd605a48
Brian Siegel <brian.siegel@carbyte.de>
2022-07-26 13:59:34 +0200

    Added beauty changes

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 // get color picker input element from id
 let colorWell = document.querySelector("#colorWell");
+let colorWellLabel = document.getElementById("colorWellLabel");
 
 chrome.storage.sync.get("color", ({ color }) => {
     colorWell.value = color;
     // change text of label
-    document.getElementById("colorWellLabel").innerHTML = colorWell.value
+    colorWellLabel.innerHTML = colorWell.value
 });
 
 // see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/color for events explanation
@@ -14,6 +15,6 @@ colorWell.addEventListener("input", setColorPickerColor, false);
 
 // save current color for later
 function setColorPickerColor() {
-    document.getElementById("colorWellLabel").innerHTML = colorWell.value
+    colorWellLabel.innerHTML = colorWell.value
     chrome.storage.sync.set({ color: colorWell.value }, function () {});
 }
